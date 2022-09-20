ed643a8c0fdc3bc8d5fbb9b17d20b85fecfe78a5
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-09-20 14:56:03 +0300

    Installed CRUD and added three buttons

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button className="button-blue">Go</button>
+        <button className="button-green">Edit</button>
+        <button className="button-red">Delete</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
