4497497e46eed692559f386adbca1497de6f438d
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-11-26 13:56:46 +0200

    local storage: get, read, write data

---
+++ Functions/localStorage.js
@@ -0,0 +1,25 @@
+function getId(key) {
+  const keyName = key + "_id";
+  let id = localStorage.getItem(keyName);
+  if (null === id) {
+    id = 0;
+  } else {
+    id = parseInt(id);
+  }
+  id++;
+  localStorage.setItem(keyName, id);
+  return id;
+}
+
+function readData(key) {
+  const data = localStorage.getItem(key);
+  if (null === data) {
+    localStorage.setItem(key, JSON.stringify([]));
+    return [];
+  }
+  return JSON.parse(data);
+}
+
+function writeData(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
