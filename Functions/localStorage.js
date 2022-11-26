ec9c194c2c331ac91b663403c4f8992eae66c5d8
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-11-26 14:10:32 +0200

    CRUD

--- Functions/localStorage.js
+++ Functions/localStorage.js
@@ -23,3 +23,32 @@ function readData(key) {
 function writeData(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
+// CRUD f-jos:
+
+export function create(key, data) {
+  const thing = readData(key);
+  data.id = getId(key);
+  thing.push(data);
+  writeData(key, thing);
+}
+
+export function read(key) {
+  return readData(key);
+}
+
+export function update(key, data, id) {
+  const thing = readData(key);
+  writeData(
+    key,
+    thing.map((t) => (t.id === id ? { ...t, ...data, id: id } : { ...t }))
+  );
+}
+
+export function destroy(key, id) {
+  const thing = readData(key);
+  writeData(
+    key,
+    thing.filter((t) => t.id !== id)
+  );
+}
